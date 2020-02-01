e78bee33bb91331b8348efe4b13d7c2282ff9713
anujkap <anujkapoor10@gmail.com>
2020-02-02 02:12:40 +0530

    Added Logo to QR Code, Removed Side Nav

--- app/src/main/java/com/anuj/devsocsignup/BottomNavActivity.java
+++ app/src/main/java/com/anuj/devsocsignup/BottomNavActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -15,34 +17,36 @@ import androidx.navigation.ui.NavigationUI;
 
 public class BottomNavActivity extends AppCompatActivity {
 
-    AppBarConfiguration mAppBarConfig;
-    ActionBarDrawerToggle toggle;
+//    AppBarConfiguration mAppBarConfig;
+//    ActionBarDrawerToggle toggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.nav_drawer_main);
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        setContentView(R.layout.activity_bottom_nav);
+//        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_timeline, R.id.navigation_map, R.id.navigation_faq, R.id.navigation_qr, R.id.navigation_sponsors, R.id.navigation_speakers, R.id.navigation_us)
                 .build();
-        mAppBarConfig = new AppBarConfiguration.Builder(R.id.navigation_timeline,R.id.navigation_qr,R.id.navigation_speakers,R.id.navigation_sponsors,R.id.navigation_us,R.id.navigation_faq)
-                .setDrawerLayout(drawer).build();
+//        mAppBarConfig = new AppBarConfiguration.Builder(R.id.navigation_timeline,R.id.navigation_qr,R.id.navigation_speakers,R.id.navigation_sponsors,R.id.navigation_us,R.id.navigation_faq)
+//                .setDrawerLayout(drawer).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupActionBarWithNavController(this,navController,mAppBarConfig);
+//        NavigationUI.setupActionBarWithNavController(this,navController,mAppBarConfig);
         NavigationUI.setupWithNavController(navView, navController);
-        NavigationUI.setupWithNavController(navView,navController);
+//        NavigationUI.setupWithNavController(navView,navController);
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfig)
-                || super.onSupportNavigateUp();
-    }
+//    @Override
+//    public boolean onSupportNavigateUp() {
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        return NavigationUI.navigateUp(navController, mAppBarConfig)
+//                || super.onSupportNavigateUp();
+//    }
 
     @Override
     public void onBackPressed() {
--- app/src/main/java/com/anuj/devsocsignup/ui/qr/QRFragment.java
+++ app/src/main/java/com/anuj/devsocsignup/ui/qr/QRFragment.java
@@ -1,6 +1,9 @@
 package com.anuj.devsocsignup.ui.qr;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,15 +11,13 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.anuj.devsocsignup.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
@@ -27,17 +28,20 @@ import static android.graphics.Color.WHITE;
 
 public class QRFragment extends Fragment {
 
-    public final static int QRcodeWidth = 500 ;
+    public final static int QRcodeWidth = 800 ;
     private FirebaseAuth mAuth;
     Bitmap bitmap;
+    TextView textView;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_qr, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
+        textView = root.findViewById(R.id.text_notifications);
         final ImageView qrcode = root.findViewById(R.id.qrcode);
+        final ImageView test = root.findViewById(R.id.testview);
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
         try {
+            assert user != null;
             bitmap = TextToImageEncode(user.getEmail());
             qrcode.setImageBitmap(bitmap);
 
@@ -52,7 +56,7 @@ public class QRFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
     }
 
-    Bitmap TextToImageEncode(String Value) throws WriterException {
+    Bitmap TextToImageEncode(String Value) throws WriterException{
         BitMatrix bitMatrix;
         try {
             bitMatrix = new MultiFormatWriter().encode(
@@ -66,7 +70,6 @@ public class QRFragment extends Fragment {
             return null;
         }
         int bitMatrixWidth = bitMatrix.getWidth();
-
         int bitMatrixHeight = bitMatrix.getHeight();
 
         int[] pixels = new int[bitMatrixWidth * bitMatrixHeight];
@@ -81,7 +84,30 @@ public class QRFragment extends Fragment {
         }
         Bitmap bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444);
 
-        bitmap.setPixels(pixels, 0, 500, 0, 0, bitMatrixWidth, bitMatrixHeight);
+        bitmap.setPixels(pixels, 0, QRcodeWidth, 0, 0, bitMatrixWidth, bitMatrixHeight);
+        Bitmap overlay = BitmapFactory.decodeResource(getResources(), R.drawable.devsoclogo);
+        bitmap = mergeBitmaps(overlay,bitmap);
         return bitmap;
     }
+
+    public Bitmap mergeBitmaps(Bitmap overlay, Bitmap bitmap) {
+
+        int height = bitmap.getHeight();
+        int width = bitmap.getWidth();
+
+        Bitmap combined = Bitmap.createBitmap(width, height, bitmap.getConfig());
+        Canvas canvas = new Canvas(combined);
+        int canvasWidth = canvas.getWidth();
+        int canvasHeight = canvas.getHeight();
+
+        canvas.drawBitmap(bitmap, new Matrix(), null);
+
+        int centreX = (canvasWidth  - overlay.getWidth()) /2;
+        int centreY = (canvasHeight - overlay.getHeight()) /2 ;
+        canvas.drawBitmap(overlay, centreX, centreY, null);
+
+        return combined;
+    }
+
 }
+
